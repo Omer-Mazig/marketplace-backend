b31f12ec84f2b25742c16828618a7b0d2f715633
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2024-09-17 12:28:00 +0300

    added services for each module

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MessagesController } from './messages.controller';
+import { MessagesService } from './providers/messages.service';
 
 @Module({
-  controllers: [MessagesController]
+  controllers: [MessagesController],
+  providers: [MessagesService]
 })
 export class MessagesModule {}
