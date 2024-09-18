74a7bcca2f9683a716a71f8fa89b5405f9bdd4de
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2024-09-18 13:10:00 +0300

    added import to user module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './providers/users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
+  exports: [UsersService],
+  imports: [TypeOrmModule.forFeature([User]), forwardRef(() => AuthModule)],
 })
 export class UsersModule {}
