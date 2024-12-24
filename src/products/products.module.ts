10ee0dc93d88ebd068831648f795f82f61bec3a1
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2024-12-24 14:36:17 +0200

    Update products.module.ts

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -8,10 +8,15 @@ import { User } from 'src/users/user.entity';
 import { AuthModule } from 'src/auth/auth.module';
 import { NotificationsModule } from 'src/notifications/notifications.module';
 import { NotificationsService } from 'src/notifications/providers/notifications.service';
+import { Notification } from 'src/notifications/notification.entity';
 
 @Module({
   controllers: [ProductsController],
   providers: [ProductsService, UsersService, NotificationsService],
-  imports: [TypeOrmModule.forFeature([Product, User]), AuthModule],
+  imports: [
+    TypeOrmModule.forFeature([Product, User, Notification]),
+    AuthModule,
+    // NotificationsModule,
+  ],
 })
 export class ProductsModule {}
