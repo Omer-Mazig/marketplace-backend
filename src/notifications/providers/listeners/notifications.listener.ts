c3a9177b78c2eb58a93269e977befd9a1e12ca65
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2024-12-27 16:53:15 +0200

    added notification events

--- src/notifications/providers/listeners/notifications.listener.ts
+++ src/notifications/providers/listeners/notifications.listener.ts
@@ -1,5 +1,5 @@
 import { OnEvent } from '@nestjs/event-emitter';
-import { Injectable } from '@nestjs/common';
+import { Injectable, RequestTimeoutException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import {
@@ -7,6 +7,8 @@ import {
   ProductUpdatedEvent,
 } from '../events/notification.events';
 import { Notification } from '../../notification.entity';
+import { Product } from 'src/products/product.entity';
+import { NotificationType } from 'src/notifications/types/notifications.type';
 
 @Injectable()
 export class NotificationsListener {
@@ -19,38 +21,62 @@ export class NotificationsListener {
   async handleProductDeletion(event: ProductDeletedEvent) {
     const { product } = event;
 
-    if (!product.wishlistUsers || product.wishlistUsers.length === 0) {
-      return;
-    }
+    if (this._hasWishlist(product)) return;
 
-    const notifications = product.wishlistUsers.map((user) =>
-      this.notificationsRepository.create({
-        user,
-        message: `The product '${product.name}' has been deleted.`,
-        type: 'product_deleted',
-      }),
+    const metadata = { productName: product.name };
+
+    await this._createAndSaveNotifications(
+      product,
+      'product.deleted',
+      metadata,
     );
-
-    await this.notificationsRepository.save(notifications);
   }
 
   @OnEvent('product.updated')
   async handleProductUpdate(event: ProductUpdatedEvent) {
     const { product } = event;
 
-    if (!product.wishlistUsers || product.wishlistUsers.length === 0) {
-      return;
-    }
+    if (this._hasWishlist(product)) return;
 
+    const metadata = { productId: product.id, productName: product.name };
+
+    await this._createAndSaveNotifications(
+      product,
+      'product.updated',
+      metadata,
+    );
+  }
+
+  private _hasWishlist(product: Product) {
+    return !product.wishlistUsers || product.wishlistUsers.length === 0;
+  }
+
+  private async _createAndSaveNotifications(
+    product: Product,
+    type: NotificationType,
+    metadata: Record<string, any>,
+  ) {
     const notifications = product.wishlistUsers.map((user) =>
       this.notificationsRepository.create({
         user,
-        message: `The product '${product.name}' has been updated.`,
-        type: 'product_updated',
-        metadata: { productId: product.id, productName: product.name },
+        type,
+        metadata,
       }),
     );
 
-    await this.notificationsRepository.save(notifications);
+    try {
+      await this.notificationsRepository.save(notifications);
+    } catch (error) {
+      console.error(
+        '[NotificationsListener - createAndSaveNotifications]',
+        error,
+      );
+      throw new RequestTimeoutException(
+        'Unable to process your request. Please try later.',
+        {
+          description: 'Error connecting to the database',
+        },
+      );
+    }
   }
 }
--- src/notifications/types/notifications.type.ts
+++ src/notifications/types/notifications.type.ts
@@ -1,8 +1,8 @@
 export const NOTIFICATION_TYPES = [
-  'product_deleted',
-  'product_updated',
-  'wishlist_batch',
-  'user_subscription',
+  'product.deleted',
+  'product.updated',
+  'wishlist.batch',
+  'user.subscription',
 ] as const;
 
 export type NotificationType = (typeof NOTIFICATION_TYPES)[number];
